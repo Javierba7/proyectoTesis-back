76039fa094d476231bca4bc729a092327c9d8d30
Javierba7 <78235573+Javierba7@users.noreply.github.com>
2021-10-11 23:54:41 -0600

    Update joi

--- validate/validateData.js
+++ validate/validateData.js
@@ -4,8 +4,8 @@ const registerValidation = data => {
     const schema = new Joi.object({
         email: Joi.string().min(6).required().email(),
         password: Joi.string().min(6).required(),
-        name: Joi.string().min(4).required(),
-        secondName: Joi.string().min(5),
+        name: Joi.string().min(3).required(),
+        secondName: Joi.string().min(3),
         lastName: Joi.string(),
         secondLastName: Joi.string()  
     }); 
